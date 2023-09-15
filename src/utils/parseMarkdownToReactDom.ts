2a74daa38d47b70bba7a765ed282edc500535ff1
flowerofmycontry <1079715793@qq.com>
2023-09-15 08:55:43 +0800

    remove some infos

--- src/utils/parseMarkdownToReactDom.ts
+++ src/utils/parseMarkdownToReactDom.ts
@@ -1,10 +1,9 @@
 // import { parse as parseMarkDown } from 'marked';
 import parse from 'html-react-parser';
-import {Marked} from "marked";
-import {markedHighlight} from "marked-highlight";
+import { Marked } from 'marked';
+import { markedHighlight } from 'marked-highlight';
 import hljs from 'highlight.js';
-import {markedEmoji, EmojiObject} from "../extensions/emoji";
-import {Octokit} from "@octokit/rest";
+import { markedEmoji, EmojiObject } from '../extensions/emoji';
 
 // const octokit = new Octokit();
 // Get all the emojis available to use on GitHub.
@@ -19,17 +18,13 @@ import {Octokit} from "@octokit/rest";
 //   marked.use(markedEmoji(options));
 // })
 
-const octokit = new Octokit({
-  auth: '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'
-});
-// Get all the emojis available to use on GitHub.
-const res = await octokit.rest.emojis.get();
+const emojisJson = import.meta.glob('../assets/emoji.json', { as: 'raw', eager: true });
 
-const emojis = res.data as Record<string, string | EmojiObject>;
+const emojis = JSON.parse(Object.values(emojisJson)[0]) as Record<string, string | EmojiObject>;
 
 const options = {
-	emojis,
-	unicode: false,
+  emojis,
+  unicode: false,
 };
 
 const marked = new Marked(
@@ -38,12 +33,11 @@ const marked = new Marked(
     highlight(code, lang) {
       const language = hljs.getLanguage(lang) ? lang : 'plaintext';
       return hljs.highlight(code, { language }).value;
-    }
-  })
+    },
+  }),
 );
 
 marked.use(markedEmoji(options));
-
 
 export default (markdown: string) => {
   return parse(marked.parse(markdown) as string);
